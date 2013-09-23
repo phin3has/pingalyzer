a4d34a5c25f8e58c97cb8d148b1f9aefbb69e96e
awhitehatter <awhitehttr@gmail.com>
2013-09-23 17:21:45 -0600

    Reverted code to python2

--- pingalyzer.py
+++ pingalyzer.py
@@ -1,15 +1,11 @@
-'''
-Author: awhitehttr@gmail.com
-desc:
-This is a little tool I'm writing to learn Python. It does (or will) analyze ping...feel free to modify or contribute.
-Also, I'm learning Pythonon the fly, so any coding best practices advice you have, please share. 
-'''
+uthor: AWhiteHatter 
+Contact: awhitehttr@gmail.com
+Description: Simple Ping script that I'm using to teach myself Python. Advice is welcomed. 
+Version: 0.0 "Getting to know the ropes"
 
+'''
+import sys, os, platform, re
 
-import sys, os, re
-#in Python3.3 there's a sick module called "ipaddress", Nix hasn't pulled 3.3 into the reps and I don't feel like forcing that on people. 
-#import ipaddress
- 
 logo = '''
  ____ ____  ____    ____   ____  _      __ __  _____    ___  ____  
 |    \    ||    \  /    | /    || |    |  |  ||     |  /  _]|    \ 
@@ -19,48 +15,36 @@ logo = '''
 |  |  |  | |  |  ||     ||  |  ||     ||     ||     ||     ||  .  \\
 |__| |____||__|__||___,_||__|__||_____||____/ |_____||_____||__|\_|
                                                                    
- 
- 
+
+
 '''
- 
-print(logo)
- 
-#Formatting in M$ an Nix Ping replies varies, for now sticking with Nix. 
-print('''\n\n***********************************************************************************
-****  Important!!!! At this time, PINGALYZER only works on Nix w/Python 3 ****
-***********************************************************************************\n\n''')
- 
-while True: 
-  i1 = input('Please enter the Host IP Address: ')
-	try:
-		#below line only works in ipaddress module in python3.3
-		#host = ipaddr.ip_address(i1)
-		reggie = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-		host = reggie.match(i1)
-		if host:
-			break
-		else:
-			print('Fail Whale! You did not enter a valid IP address YO!')
-			continue
-	except:
-		print('Fail Whale! You did not enter a valid IP address YO!')
-		continue
- 
-while True:
-	try:
-		pgx = input('How many times would you like to ping this host? \nEnter [c] for continuous: ')
-		if pgx[0] == 'c':
-			print('\nWe will now ping {0} continuously...'.format(i1))
-			os.system('/bin/ping {0}'.format(i1))
-			break
-		else:
-			pgx = int(pgx)
-			print('\nWe will now ping {0} {1} times...'.format(i1,pgx))
-			os.system('ping {0} -c {1}'.format(i1, pgx))
-			break
-	except:
-    		print('\nPlease enter either a valid number or "c".\n')
-    		continue
- 
- 
+
+print logo
+
+ostype = platform.system()
+print "You are using " + ostype + "\n"
+
+print "What target would you like to ping today? \n"
+
+target = raw_input('--> ')
+
+#validate target is an actual IP Address
+reggie = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+host = reggie.match(target)
+
+if host:
+	print ""
+else:
+	print "\nFail Whale, You didn't a enter a valid IPv4 Address YO!"
+	raw_input('\nHit enter to exit\n\n')
+	exit()
+
+print "How many times would like to ping " + target + "?\n"
+
+hits = int(raw_input('--> '))
+
+print "\nWe will now ping %r a total of %r times:\n" % (target, hits)
+
+#dirty exit from the script
+raw_input('\nHit enter to exit\n\n')
 exit()
